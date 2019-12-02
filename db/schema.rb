ada984f4f2b0eb07e60ead36c77cf0924ecab729
johnrozario <jj.rozario@gmail.com>
2019-12-02 10:06:04 +0530

    latest chnges 2

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_054309) do
+ActiveRecord::Schema.define(version: 2019_12_02_023544) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -88,6 +88,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_054309) do
     t.date "plan_order_dt"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "lot_size"
   end
 
   create_table "trn_prod_plan_masters", force: :cascade do |t|
